3f0afac55937f72eee8a9c73bbc991f86ab62280
Bing <panbing0105@gmail.com>
2016-08-22 23:51:03 +0800

    the fucking code.

--- app/src/main/java/com/icegod/calendar/MainActivity.java
+++ app/src/main/java/com/icegod/calendar/MainActivity.java
@@ -2,22 +2,62 @@ package com.icegod.calendar;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.CalendarView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+import java.io.Serializable;
 
 public class MainActivity extends Activity {
+
+    private GridView mGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CalendarView calendarView = (CalendarView) findViewById(R.id.cal_view);
+        mGridView = (GridView) findViewById(R.id.cal_grid);
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+    }
 
-            }
-        });
+    class CalendarItem implements Serializable {
+        int year;
+        int month;
+        int day;
+        String nongli;
+        
+    }
+
+    class CalendarGridAdapter extends BaseAdapter {
+
+        public CalendarGridAdapter() {
+
+        }
+
+        public void updateData() {
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }

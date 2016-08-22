d1b6e3fd82d37e635aeb913f99a8499f725a6aec
Pan Bing <panbing@le.com>
2016-08-22 18:25:35 +0800

    modify commit.

--- app/src/main/java/com/icegod/calendar/MainActivity.java
+++ app/src/main/java/com/icegod/calendar/MainActivity.java
@@ -1,13 +1,23 @@
 package com.icegod.calendar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.cal_view);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
     }
 }
